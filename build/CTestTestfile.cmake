69ec37e0240a0309d0aed2ec80af30b541fe5dee
0xIrakli <butu.irakli@gmail.com>
2022-11-18 00:37:37 +0400

    Edit gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/User/Desktop/Projects/WebcamToText cpp
-# Build directory: C:/Users/User/Desktop/Projects/WebcamToText cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
